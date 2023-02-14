06e77c4d4dab099621b9cad11a593a04b0743e32
Jaren <84934521+Apozem@users.noreply.github.com>
2023-02-13 21:06:56 -0800

    Update housePricePredictions.R
    Added basic decision tree with its predictions for the test data

--- housePricePredictions.R
+++ housePricePredictions.R
@@ -1,8 +1,13 @@
+#libraries
+library(rpart)
+library(rpart.plot)
+
 #retains accuracy of decimal places
 options(digits = 10)
 
-#read in data of train file, from SAME DIRECTORY AS THIS FILE
+#read in data of train file and test file, from SAME DIRECTORY AS THIS FILE
 data <- read.csv("train.csv")
+test <- read.csv("test.csv")
 
 #data separated into own vectors
 posted <- as.character(unlist(data[1]))
@@ -27,8 +32,22 @@ data$is_outlier <- ifelse(data$TARGET.PRICE_IN_LACS. %in% boxplot.stats(data$TAR
 #remove rows with outliers
 data_no_outlier <- subset(data,is_outlier==0)
 
+#finds and removes sqft outliers
 outliers_sqft <- boxplot(data_no_outlier$SQUARE_FT, plot = FALSE)$out
-
 data_no_outlier$is_outlier <- ifelse(data_no_outlier$SQUARE_FT %in% boxplot.stats(data_no_outlier$SQUARE_FT)$out, 1, 0)
-
 data_no_outlier <- subset(data_no_outlier,is_outlier==0)
+
+#clean data
+clean_data <- data_no_outlier[ , !names(data_no_outlier) %in% c("is_outlier", "ADDRESS")]
+
+#create the basic decision tree
+tree <- rpart(TARGET.PRICE_IN_LACS. ~., data=clean_data)
+
+#create target price column and fill with zeros
+test$TARGET.PRICE_IN_LACS. <- c(0)
+
+#preditctions
+prd <- predict(tree, test)
+
+#makes table with predictions
+table_p <- table(test$TARGET.PRICE_IN_LACS., prd)
